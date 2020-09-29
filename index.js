06bb0e4ffeabb9b02715f793fc0323a5d4ad4e4d
Fernando <30844912+FernandoE99@users.noreply.github.com>
2020-09-29 01:04:54 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require('constants');
 const Discord = require('discord.js');
 const {prefix, token} = require('./config.json');
 const client = new Discord.Client();
